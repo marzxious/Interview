8e7c4b0d16b8452b641f66050135b64f44cb1764
qyy <yanyuan.qin@uconn.edu>
2020-01-05 01:31:35 -0500

    Update Serialize and Deserialize BST.py

--- Amazon/Onsite/Python/Serialize and Deserialize BST.py
+++ Amazon/Onsite/Python/Serialize and Deserialize BST.py
@@ -8,11 +8,11 @@
 
 # Choose delimiters to separate the values in the encoded string.
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Codec:
     def serialize(self, root):
@@ -50,3 +50,37 @@ class Codec:
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
+# 先序遍历
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = self.preorder(root)
+        ans = ','.join(res)
+        return ans
+    
+    def preorder(self, root): # Root, L, R
+        if root is None: return []
+        left = self.preorder(root.left)
+        right = self.preorder(root.right)
+        return [str(root.val)] + left + right
+                
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        :type data: str
+        :rtype: TreeNode
+        """
+        if len(data) == 0: return None
+        data = map(int, data.split(','))
+        data = collections.deque(data)
+        return self.preorder(data, float('-inf'), float('inf'))
+        
+    def preorder(self, data, l, u): # 从开始处理，按照Root, L, R
+        if len(data) == 0 or not l < data[0] < u: return None
+        val = data.popleft()
+        root = TreeNode(val)
+        root.left = self.preorder(data, l, val)
+        root.right = self.preorder(data, val, u)
+        return root
